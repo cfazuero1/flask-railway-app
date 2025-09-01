af1d0fbbccf8f4413312f981572bd5488df7756f
cfazuero1 <cristhianfazuero@gmail.com>
2025-09-01 23:41:56 +1000

    email form

--- app.py
+++ app.py
@@ -175,6 +175,10 @@ def ws_ssh(ws):
 def about():
     return render_template("about.html")
 
+@app.route("/thanks")
+def thanks():
+    return render_template("thanks.html")
+
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
